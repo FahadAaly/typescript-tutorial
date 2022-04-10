2fde4bd45ab53112c40043ddfa3b7f964bda9e97
FahadAaly <ali-fahad1234@hotmail.com>
2022-04-10 14:42:14 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
+
+// let name: string;
+// let age: number | string; // union
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string]; // tupil
+
+// let printName: (name: string) => void;
+
+// type Person = {
+//   name: string;
+//   age: number;
+// };
+
+// let person: Person = {
+//   name: "Fahad",
+//   age: 25,
+// };
+
+// let lotsOfPeople: Person[];
 
 function App() {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Taskify</h1>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
